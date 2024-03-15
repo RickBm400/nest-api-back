71a0b70dcd9d1d018215bdcc60629f7b509fcbc3
RickBm400 <rjbm29@gmail.com>
2024-03-14 20:32:21 -0500

    feat: new app controller & module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { dogsController } from './dogs/controller/.controller';
+import { dogsController } from './dogs/controller/dogs.controller';
 import { DogsModule } from './dogs/dogs.module';
+import { AppController } from './app.controller';
+import { dogService } from './dogs/service/service.service';
 @Module({
   imports: [DogsModule],
-  controllers: [dogsController],
+  controllers: [dogsController, AppController],
+  providers: [dogService]
 })
 export class AppModule { }
